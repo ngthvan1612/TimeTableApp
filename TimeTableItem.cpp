a8ba83267763e13f8565e9f2114fa793a98e3f0b
ngthvan1612 <ngthvan1612@gmail.com>
2023-02-08 16:16:05 +0700

    Tạo lớp TimeTableItem

---
+++ TimeTableItem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nguyen Thanh Van on 08/02/2023.
+//
+
+#include "TimeTableItem.h"
---
+++ TimeTableItem.h
@@ -0,0 +1,14 @@
+//
+// Created by Nguyen Thanh Van on 08/02/2023.
+//
+
+#ifndef TIMETABLEAPP_TIMETABLEITEM_H
+#define TIMETABLEAPP_TIMETABLEITEM_H
+
+
+class TimeTableItem {
+
+};
+
+
+#endif //TIMETABLEAPP_TIMETABLEITEM_H
